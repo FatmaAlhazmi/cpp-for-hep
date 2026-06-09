e118d64e8130919deb800f18c6fe45bc1bc65b62
Phatimah <phatimah.alhazmi@gmail.com>
2026-06-09 07:08:30 +0300

    Add lesson 9 headers

---
+++ cpp/09_headers/task_solution_four_vector.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+struct FourVector {
+    double energy;
+    double px;
+    double py;
+    double pz;
+};
+
+double momentum_squared(FourVector p);
+double invariant_mass_squared(FourVector p);
+double dot(FourVector a, FourVector b);
