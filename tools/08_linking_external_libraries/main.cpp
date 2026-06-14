2f653d3fca8322f141fa40cabfd64e1e6594da04
Phatimah <phatimah.alhazmi@gmail.com>
2026-06-14 22:26:22 +0300

    Add local library linking example

---
+++ tools/08_linking_external_libraries/main.cpp
@@ -0,0 +1,11 @@
+#include "simple_math.hpp"
+
+#include <iostream>
+
+int main() {
+    double answer = add(2.0, 3.0);
+
+    std::cout << "answer = " << answer << "\n";
+
+    return 0;
+}
---
+++ tools/08_linking_external_libraries/src/simple_math.cpp
@@ -0,0 +1,5 @@
+#include "simple_math.hpp"
+
+double add(double a, double b) {
+    return a + b;
+}
